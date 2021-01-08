38995fe37794e9b9bb8816df9ba1cc3260977162
zonanjaro86 <zonanjaro86@gmail.com>
2021-01-08 11:32:35 +0900

    delete

--- abc137/abc137_a.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-a, b = map(int, input().split())
-print(max(a + b, a - b, a * b))
--- abc137/abc137_b.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-k, x = map(int, input().split())
-print(' '.join(map(str, range(x - k + 1, x + k))))
