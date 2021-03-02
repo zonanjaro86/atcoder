08529687190aadf7678f8c827b7f0cf8c7f35ada
zonanjaro86 <zonanjaro86@gmail.com>
2021-03-02 16:59:01 +0900

    refactored

--- python/src/abc193_e.py
+++ python/src/abc193_e.py
@@ -3,39 +3,45 @@ def gcd(m, n):
         m, n = n, m % n
     return m
 
-def extgcd(a, b):
-    if b:
-        d, y, x = extgcd(b, a % b)
-        y -= (a // b) * x
-        return d, x, y
-    return a, 1, 0
+def egcd(a, b):
+    """拡張ユークリッド互助法
+    px + qy = gcd(p, q) = dを満たすdとxとyを返す
+    """
+    if a == 0:
+        return b, 0, 1
+    g, y, x = egcd(b % a, a)
+    return g, x - (b // a) * y, y
 
-def crt(a, b, mod1, mod2):
-    # px + qy = d = gcd(p, q)
-    d, p, _ = extgcd(mod1, mod2)
-    if (b - a) % d != 0:
-        return -1, 0
-    lcm = mod1 * (mod2 // d)
-    r = a + mod1 * ((b - a) // d) * p
-    r %= lcm
-    # print('a:{}, b:{}, mod1:{}, mod2:{}'.format(a, b, mod1, mod2))
+def chineseRem(b1, m1, b2, m2):
+    """中国剰余定理(2次元)
+    x ≡ b1 mod m1 ∧ x ≡ b2 mod m2 <=> x ≡ r mod m
+    となる(r, m)を返す
+    解なしの場合は(0, -1)
+    """
+    d, p, q = egcd(m1, m2)
+    if (b1 - b2) % d != 0:
+        return 0, -1
+    m = m1 * (m2 // d) # m = lcm(m1, m2)
+    tmp = (b2-b1) // d * p % (m2 // d)
+    r = (b1 + m1 * tmp) % m
+    # print('a:{}, b:{}, C:{}, D:{}'.format(a, b, C, moDd2))
     # print('r:{}, lcm:{}'.format(r, lcm))
-    return r, lcm
+    return r, m
 
 
 def solve():
     X, Y, P, Q = map(int, input().split())
 
+    # 周期
+    C = 2 * (X + Y)
+    D = P + Q
+
     ans = float("inf")
-    for t1 in range(X, X+Y):
-        for t2 in range(P, P+Q):
-            # t = t1 mod 2*(X+Y)
-            # t = t2 mod P+Q
-            t, lcm = crt(t1, t2, 2*(X+Y), P+Q)
-            if lcm == 0:
+    for a in range(X, X+Y):
+        for b in range(P, P+Q):
+            t, lcm = chineseRem(a, C, b, D)
+            if lcm == -1:
                 continue
-            if t < t1:
-                t += lcm
             ans = min(ans, t)
     if ans == float("inf"):
         print("infinity")
