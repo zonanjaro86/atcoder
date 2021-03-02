e8556bd6437100ddcfdadbe57a08ab7b8babf719
zonanjaro86 <zonanjaro86@gmail.com>
2021-03-02 16:15:43 +0900

    refactored

--- python/src/abc193_d.py
+++ python/src/abc193_d.py
@@ -1,40 +1,39 @@
-from collections import Counter
+# スコアの計算
+def score(str):
+    a = [0] * 10
+    for s in str:
+        a[int(s)] += 1
+    score = 0
+    for i in range(1, 10):
+        score += i * 10 ** a[i]
+    return score
 
-K = int(input())
-S = input()
-T = input()
+def main():
+    K = int(input())
+    S = input()
+    T = input()
 
-s_cnt = Counter(S[:4])
-t_cnt = Counter(T[:4])
+    # 山札の各数字の残り枚数
+    rest = [K] * 10
+    for i in range(4):
+        rest[int(S[i])] -= 1
+        rest[int(T[i])] -= 1
 
-# 裏向きのカードを除いた各数字の点数
-s_base = [i * 10 ** s_cnt[str(i)] for i in range(1, 10)]
-t_base = [i * 10 ** t_cnt[str(i)] for i in range(1, 10)]
+    # 勝ちパターンを計算
+    cnt = 0
+    for i in range(1, 10):
+        s_score = score(S[:4] + str(i))
+        for j in range(1, 10):
+            t_score = score(T[:4] + str(j))
+            if s_score > t_score:
+                if i == j:
+                    cnt += rest[i] * (rest[i] - 1)
+                else:
+                    cnt += rest[i] * rest[j]
 
-# 裏向きのカードが1～9のときの合計点数
-s_point = [sum(s_base) + s_base[i-1]*9 for i in range(1, 10)]
-t_point = [sum(t_base) + t_base[i-1]*9 for i in range(1, 10)]
+    # 全パターンで割って確率を計算
+    total = (9 * K - 8) * (9 * K - 9)
+    print(cnt / total)
 
-# 裏向きのカードを除いた各数字の残り枚数
-c = Counter({'1': K, '2': K, '3': K, '4': K, '5': K, '6': K, '7': K, '8': K, '9': K}) - s_cnt - t_cnt
-# print(c)
-# 2 * K - 8
-
-p_win = 0
-for i in range(1, 10):
-    if c[str(i)] == 0:
-        continue
-    for j in range(1, 10):
-        # もう残ってない場合は無視
-        if c[str(i)] == 0 or c[str(j)] == 0:
-            continue
-        # 残り一枚で同じ数字を引いた場合も無視
-        if i == j and c[str(i)] <= 1:
-            continue
-        # このパターンにたどり着く場合の数
-        p = c[str(i)] * c[str(j)] if i != j else c[str(i)] * (c[str(i)]-1)
-        # 勝ち負け判定
-        if s_point[i-1] > t_point[j-1]:
-            p_win += p
-
-print(p_win / ((9*K-8)*(9*K-9)))
+if __name__ == '__main__':
+    main()
